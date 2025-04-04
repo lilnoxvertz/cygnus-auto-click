4c2ffbdd89ff1e9eb4e945d562ff46da5b192afd
noxx <68088454+lilnoxvertz@users.noreply.github.com>
2025-04-05 00:11:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 (function start() {
+
+    const targetElement = document.querySelectorAll(".bg-100")
+
+    if(!targetElement) {
+        console.log("target not found")
+        return
+    }
+    
     const getRandomNumber = (min, max) => {
         return Math.floor(Math.random() * (max - min + 1)) + min
     }
@@ -20,9 +28,12 @@
     }
 
     async function start() {
-        const delay = getRandomNumber(250, 1000) //atur delay disini 250 = minimal, 500 - max
-        const x = getRandomNumber(600, 920)
-        const y = getRandomNumber(130, 490)
+        const delay = getRandomNumber(250, 1000) //atur delay disini 250 - minimal, 500 - max
+        
+        const getTargetCord = targetElement[1].getBoundingClientRect()
+        const x = getTargetCord.left + Math.random() * getTargetCord.width
+        const y = getTargetCord.top + Math.random() * getTargetCord.height
+        
         await simulateMouse(x, y)
         setTimeout(start, delay)
         console.log(`time between click: ${delay}`)
